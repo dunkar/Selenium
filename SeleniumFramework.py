5cfc29108cd748edc05de48030b09ea5e5e31ca9
John <git@jnkdahl.com>
2015-07-02 22:13:05 -0500

    Added the locator converter method and unit tests.

--- SeleniumFramework.py
+++ SeleniumFramework.py
@@ -2,42 +2,78 @@
 # -*- coding: utf-8 -*- 
 
 import time
-import os
-from datetime                                   import datetime
+# import os
+# from datetime                                   import datetime
 from selenium                                   import webdriver
-from selenium.webdriver.common.action_chains    import ActionChains
+# from selenium.webdriver.common.action_chains    import ActionChains
 from selenium.webdriver.common.by               import By
-from selenium.webdriver.common.keys             import Keys
-from selenium.webdriver.support                 import expected_conditions
-from selenium.webdriver.support.ui              import Select
-from selenium.webdriver.support.ui              import WebDriverWait
-from selenium.webdriver.remote.webelement       import WebElement
-from selenium.webdriver.remote.webdriver        import WebDriver
-from selenium.common                            import exceptions
-from selenium.common.exceptions                 import NoSuchElementException
-from selenium.common.exceptions                 import NoAlertPresentException
-from selenium.common.exceptions                 import TimeoutException
-
+# from selenium.webdriver.common.keys             import Keys
+# from selenium.webdriver.support                 import expected_conditions
+# from selenium.webdriver.support.ui              import Select
+# from selenium.webdriver.support.ui              import WebDriverWait
+# from selenium.webdriver.remote.webelement       import WebElement
+# from selenium.webdriver.remote.webdriver        import WebDriver
+# from selenium.common                            import exceptions
+# from selenium.common.exceptions                 import NoSuchElementException
+# from selenium.common.exceptions                 import NoAlertPresentException
+# from selenium.common.exceptions                 import TimeoutException
+# 
 
 class Driver(object):
     def __init__(self):
-        self.AC = ActionChains
+        pass
+        # self.WD = webdriver
+        # self.AC = ActionChains
         self.By = By
-        self.EC = expected_conditions
-        self.EX = exceptions
-        self.Keys = Keys
-        self.Select = Select
-        self.WD = webdriver
-        self.WDWait = WebDriverWait
-        self.WebElement = WebElement
-        self.WebDriver = WebDriver
+        # self.EC = expected_conditions
+        # self.EX = exceptions
+        # self.Keys = Keys
+        # self.Select = Select
+        # self.WDWait = WebDriverWait
+        # self.WebElement = WebElement
+        # self.WebDriver = WebDriver
         
     def open(self):
-        pass
+        self.browser = webdriver.Chrome()
+        return self.browser
         
     def close(self):
-        pass
-        
+        try:
+            self.browser.quit()
+        except:
+            self.browser.close()
+    
+    def goto(self, url):
+        try:
+            self.browser.get(url)
+        except:
+            raise
+    
+    def convert_locator(self, locator_string):
+        '''Given a locator string of the format:
+            type=value,
+        return the valid locator pieces.'''
+        By = self.By
+        locator_map = {
+            'id'    : By.ID,
+            'name'  : By.NAME,
+            'css'   : By.CSS_SELECTOR,
+            'class' : By.CLASS_NAME,
+            'link'  : By.LINK_TEXT,
+            'plink' : By.PARTIAL_LINK_TEXT,
+            'tag'   : By.TAG_NAME,
+            'xpath' : By.XPATH,
+            'window': 'window',
+            'frame' : 'frame',
+        }
+        locator_type  = locator_string[:locator_string.find('=')].strip()
+        locator_value = locator_string[locator_string.find('=') + 1:].strip()
+        try:
+            return (locator_map[locator_type], locator_value)
+        except KeyError:
+            return False
+    
+    
     
 class Object( object ):
     pass
--- sf_ut.py
+++ sf_ut.py
@@ -7,16 +7,27 @@ import SeleniumFramework as sf
 class TestPlan( unittest.TestCase ):
     def setUp( self ):
         self.driver = sf.Driver()
+        self.driver.open()
         
     def tearDown( self ):
+        self.driver.close()
         del self.driver
 
-    def test_001_open_close_browser(self):
-        self.assertTrue(False)
+    def test_001_open_url(self):
+        d = self.driver
+        d.goto('https://duckduckgo.com/')
+        self.assertTrue('DuckDuckGo' in d.browser.title)
         
-    def test_002_navigate_to_webpage(self):
-        self.assertTrue(False)
+    def test_002_convert_locator_successful(self):
+        cl = self.driver.convert_locator
+        self.assertTrue(cl('id=abc')        == (self.driver.By.ID, 'abc'))
+        self.assertTrue(cl('window=001')    == ('window','001'))
+        self.assertTrue(cl('frame=xyz')     == ('frame','xyz'))
+        
+    def test_003_convert_locator_failure(self):
+        cl = self.driver.convert_locator
+        self.assertFalse(cl('sound=quack'))
 
-
+        
 if __name__ == '__main__':
     unittest.main( warnings='ignore' )
